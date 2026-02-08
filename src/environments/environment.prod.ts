d5d3ea9dd6273448dc039921e7c2439cf7e1cda3
mina <mandymina789@gmil.com>
2026-02-09 00:11:30 +0200

    test4

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://talkhashop.runasp.net/api',
+  apiUrl: 'https://talkhashop.runasp.net/api',
   //jwtKey: 'YourSuperSecretKeyForJwtTokenGeneration2026!'
 };
